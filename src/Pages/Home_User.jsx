aef390d78c8792168bea42564a9a91b8286a9fb3
Jérémy Bosqui <83767390+jeremybosqui@users.noreply.github.com>
2022-12-11 23:32:10 +0100

    update and create modelisation classe

--- src/Pages/Home_User.jsx
+++ src/Pages/Home_User.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import home from "../Assets/accueil_image.png"
 
+class HeaderNav extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { appName:"Sportsee"}
+    }
+    static getOtherColor(props) {
+        return {appName: props}
+    }
+    render() {
+        return <div>
+            <h1> Bienvenue sur l'application {this.state.appName}</h1>
+            <img src={home} alt={"img_app_presentation"} style={{width:"800px", height:"600px", marginTop:"100px", marginLeft:"150px"}}/>
+        </div>
+    }
+}
+export default HeaderNav
 
-export default function Home (){
+/*export default function Home (){
 
-    /*const [locations, setLocations] = useState([]);
+    /!*const [locations, setLocations] = useState([]);
 
     useEffect(() => {
         fetch('/Datas/DataMain.json')
@@ -14,7 +30,7 @@ export default function Home (){
             .then((data) => {
                 setLocations(data)
             })
-    }, [])*/
+    }, [])*!/
 
   return (
     <div>
@@ -23,5 +39,5 @@ export default function Home (){
 
 
   )
-}
+}*/
 
